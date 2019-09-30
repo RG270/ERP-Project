b3753953e85dd2be1e36804860be750547fadca0
Pankaj Singh <spankaj2n@gmail.com>
2019-10-01 00:26:10 +0530

    connected react with express

--- client/src/Names/Names.js
+++ client/src/Names/Names.js
@@ -1,36 +1,31 @@
-import React, {Component} from "React";
+import React, {Component} from "react";
 
 class Names extends Component {
 	
    constructor(props){
       super(props);
       this.state = {
-       names: []
+       names: [],
       }
-   };
+   }
    
    componentDidMount(){
 
    fetch("/")
    .then(res=>res.json())
-   .then((names)=> {this.setState({names: names});});
+   .then((names)=> {this.setState({names}, ()=>console.log('here'));});
  
-   };
+   }
 
    render(){
-
-
    	return (
+      <div>
       <ul>
-         {this.state.names.map(name=>{
-         	 <li>{name}</li>
-         })
-     }
+        { this.state.names.map((name)=>
+          <li>{name.name} </li>
+        )}
      </ul>
-
-     
-
-   		);
+      </div>);
    }
 
 }
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
-
+var cors = require('cors');
+app.use(cors());
 app.get('/', (req, res)=>{
 	const names = [{name: "someone" }, {name: "someoneelse"}];
 	res.json(names);
 });
 
-app.listen(3001 , process.env.IP, (req, res) => {
+app.listen(3001 , (req, res) => {
     console.log('server is listening');
 });
 

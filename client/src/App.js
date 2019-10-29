c07d513b21bb54ef07f51af37f2bb817823e0fac
Pankaj Singh <spankaj2n@gmail.com>
2019-10-29 21:46:40 +0530

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import Names from './components/Names/Names';
-import SideBar from './components/SideBar/SideBar';
-import NavBar from './components/NavBar/NavBar';
+
 class App extends Component {
   render(){
     return (
     <div className = "App">
       
-      <NavBar/>
-      <SideBar/>
-      <Names/>
+     
     </div>
   );
 }
